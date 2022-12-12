7a0beb3e1bca5865e97e44b91f2bdb059598a988
uganewaththa <uganewaththa@controlunion.com>
2022-12-12 12:12:55 +0530

    fixes

--- src/main/java/com/cusl/projetctsanta/ProjectSantaApplication.java
+++ src/main/java/com/cusl/projetctsanta/ProjectSantaApplication.java
@@ -1,9 +1,7 @@
 package com.cusl.projetctsanta;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -12,8 +10,5 @@ public class ProjectSantaApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectSantaApplication.class, args);
 	}
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(Application.class);
-	}
+
 }
