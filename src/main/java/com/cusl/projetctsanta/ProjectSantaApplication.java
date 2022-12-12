dafcb18eee28a24d342d444bb7111b573cca1fb6
uganewaththa <uganewaththa@controlunion.com>
2022-12-12 12:17:10 +0530

    fixes

--- src/main/java/com/cusl/projetctsanta/ProjectSantaApplication.java
+++ src/main/java/com/cusl/projetctsanta/ProjectSantaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectSantaApplication extends SpringBootServletInitializer {
+public class ProjectSantaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectSantaApplication.class, args);
